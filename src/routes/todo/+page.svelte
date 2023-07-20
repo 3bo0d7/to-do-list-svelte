8aca6c4b405f0c05c7a4e64d861439975bfb00a2
3bo0d7 <aboodythunder@gmail.com>
2023-07-20 13:10:57 +0400

    feat:delete button

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -7,12 +7,48 @@
   export let data
 
   let times = 0
+  /*
+  function deleteTask(id) {
+    tasks = task.filter((task) => task.id !== id)
+  }
+*/
+  function swapStates() {
+    myTasks.completed = !myTasks.completed
+  }
+
+  async function changeStatus(task) {
+    const data = { id: task.id, completed: !task.completed }
+
+    const response = await fetch('?/changeStatus', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+    console.log('response from server', response.status, response.body)
+  }
+
+  async function deleteTask(task) {
+    const response = await fetch('?/deleteTask', {
+      method: 'POST',
+      body: JSON.stringify(task),
+    })
+    console.log('delete row response ', response.status, response.body)
+    location.reload()
+  }
 </script>
 
 <h1>My tasks</h1>
 <p>Here you can create a to-do list</p>
 
-<form method="POST">
+<form method="POST" action="?/createTask">
+  <label class="mb-3">
+    Task Name
+    <input name="taskName" type="text" class="form-control" />
+  </label>
+  <br />
+  <button class="btn btn-primary">Create Task</button>
+</form>
+
+<form method="POST" action="?/changeStatus">
   <label class="mb-3">
     Task Name
     <input name="taskName" type="text" class="form-control" />
@@ -21,6 +57,7 @@
 
   <button class="btn btn-primary">Create Task</button>
 </form>
+
 <ul>
   {#each data.tasks as task}
     <li>{task.id}, {task.text}, {task.completed}</li>
@@ -32,6 +69,7 @@
     <th>id</th>
     <th>name</th>
     <th>status</th>
+    <th>Deletetask</th>
   </tr>
   <br />
 
@@ -39,7 +77,18 @@
     <tr>
       <td>{task.id}</td>
       <td>{task.text}</td>
-      <td>{task.completed}</td>
+      <td
+        ><div class="col-4">{task.completed}</div>
+        <button class="btn btn-primary" on:click={() => changeStatus(task)}
+          >Change Status</button
+        ></td
+      >
+
+      <td
+        ><button class="btn btn-primary" on:click={() => deleteTask(task)}
+          >Delete</button
+        >
+      </td>
     </tr>
   {/each}
 </table>
@@ -54,7 +103,13 @@
   <input type="radio" id="javascript" name="fav_language" />
   <label for="javascript">JavaScript</label>
 </form>
-
+<!-- 
+{#if myTasks.completed}
+  <p>True</p>
+{:else}
+  <p>False</p>
+{/if}
+-->
 <!-- 
 <App1 name={'Peter'}>In between</App1>
 <TodoList taskname={'Home Task'} />
